4e9aef6a12d4a81b70cbade6c02bfe1959050f93
chu-david <dchu@emmauscc.sa.edu.au>
2022-10-31 15:31:39 +1030

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+let distance = 0
+Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
 basic.forever(function () {
-	
+    distance = Kitronik_Move_Motor.measure()
+    if (distance >= 5) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+        basic.pause(200)
+    } else {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 20)
+        basic.pause(100)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 20)
+        basic.pause(20)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+        basic.pause(20)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 20)
+        basic.pause(20)
+    }
 })
