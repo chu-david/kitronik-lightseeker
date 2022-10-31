605f643c58e8034ac888e9b2ac8b568fd7adabcd
chu-david <dchu@emmauscc.sa.edu.au>
2022-10-31 15:37:20 +1030

    triangular algorithm

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 let distance = 0
 Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 20)
+basic.pause(10)
 basic.forever(function () {
     distance = Kitronik_Move_Motor.measure()
     if (distance >= 5) {
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
         basic.pause(200)
     } else {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 20)
-        basic.pause(100)
         Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 20)
         basic.pause(20)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
